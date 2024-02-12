bf5e2593641dae95ea318a94c06c823d8eb7fc76
pavanmahesh1518 <159711431+pavanmahesh1518@users.noreply.github.com>
2024-02-12 15:59:39 +0530

    New lines are added

---
+++ hello/Caluclator.java
@@ -0,0 +1,13 @@
+class Caluclator{
+    private int ans;
+
+    public int add(int a ){
+        ans += a;
+        return ans;
+    }
+
+    public int sub(int b){
+        ans -= a;
+        return ans;
+    }
+}
